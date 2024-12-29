725541f88fc8bb0f7922b86a4816f513df0885af
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-28 21:36:48 -0800

    test(receiver): Add message type filtering tests (#49)
    Add test for message type filtering
    
    Add a test to verify that the receiver client correctly filters and records messages based on their type, specifically checking that data messages are stored while other message types (like typing messages) are ignored.
    
    closes #39

--- pkg/receiver/client_test.go
+++ pkg/receiver/client_test.go
@@ -2,12 +2,19 @@
 package receiver
 
 import (
+	"context"
 	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"strconv"
 	"testing"
+	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 //nolint:gochecknoglobals
@@ -92,4 +99,94 @@ func TestPop(t *testing.T) {
 			assert.Equal(t, want, *c.Pop())
 		}
 	})
+}
+
+func TestRecordMessageTypes(t *testing.T) {
+	t.Parallel()
+
+	ch := make(chan Message)
+	trs := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		upgrader := websocket.Upgrader{}
+
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			t.Errorf("upgrade websocket: %v", err)
+
+			return
+		}
+		defer conn.Close()
+
+		for msg := range ch {
+			if err := conn.WriteJSON(msg); err != nil {
+				t.Errorf("write message: %v", err)
+
+				return
+			}
+		}
+	}))
+
+	defer trs.Close()
+
+	uri, err := url.Parse(trs.URL)
+	require.NoError(t, err)
+
+	uri.Scheme = "ws"
+
+	client, err := New(newContext(), uri, MessageTypeDataMessage.String())
+	require.NoError(t, err)
+
+	go func(t *testing.T) {
+		t.Helper()
+
+		assert.NoError(t, client.ReceiveLoop())
+	}(t)
+
+	var (
+		msgStr string
+		msg    Message
+	)
+
+	// ensure no messages to pop at the beginning
+	assert.Nil(t, client.Pop())
+
+	// send in a message that is a data message, what we are looking for
+	msgStr = "test1"
+	msg = Message{
+		Envelope: Envelope{
+			DataMessage: &DataMessage{
+				Message: &msgStr,
+			},
+		},
+	}
+
+	ch <- msg
+
+	// wait for the messages to be recorded
+	time.Sleep(100 * time.Millisecond)
+
+	if rm := client.Pop(); assert.NotNil(t, rm) {
+		assert.Equal(t, msg, *rm)
+	}
+
+	// now make sure the queue is empty again
+	assert.Nil(t, client.Pop())
+
+	// send a new non-data message
+	ch <- Message{
+		Envelope: Envelope{
+			TypingMessage: &TypingMessage{},
+		},
+	}
+
+	// wait for the messages to be recorded or more specifically ignored
+	for i := 0; i < 3; i++ {
+		time.Sleep(100 * time.Millisecond)
+		assert.Nil(t, client.Pop())
+	}
+}
+
+func newContext() context.Context {
+	return zerolog.
+		New(io.Discard).
+		WithContext(context.Background())
 }
